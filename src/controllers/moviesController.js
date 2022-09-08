7428c5c92fef713791c39cc499c5aa42822c1677
andrechacon <achacon190@gmail.com>
2022-09-08 02:44:41 -0300

    primero

---
+++ src/controllers/moviesController.js
@@ -0,0 +1,107 @@
+const db = require('../database/models');
+const sequelize = db.sequelize;
+const {validationResult} = require('express-validator');
+
+//Otra forma de llamar a los modelos
+const Movies = db.Movie;
+
+const moviesController = {
+    'list': (req, res) => {
+        db.Movie.findAll()
+            .then(movies => {
+                res.render('moviesList.ejs', {movies})
+            })
+    },
+    'detail': (req, res) => {
+        db.Movie.findByPk(req.params.id)
+            .then(movie => {
+                res.render('moviesDetail.ejs', {movie});
+            });
+    },
+    'new': (req, res) => {
+        db.Movie.findAll({
+            order : [
+                ['release_date', 'DESC']
+            ],
+            limit: 5
+        })
+            .then(movies => {
+                res.render('newestMovies', {movies});
+            });
+    },
+    'recomended': (req, res) => {
+        db.Movie.findAll({
+            where: {
+                rating: {[db.Sequelize.Op.gte] : 8}
+            },
+            order: [
+                ['rating', 'DESC']
+            ]
+        })
+            .then(movies => {
+                res.render('recommendedMovies.ejs', {movies});
+            });
+    }, 
+    add: function (req, res) {
+        res.render('moviesAdd')
+    },
+    create: function (req, res) {
+        db.Movie.create({
+            title: req.body.title,
+            rating: req.body.rating,
+            length: req.body.length,
+            awards: req.body.awards,
+            release_date: req.body.release_date,
+        })
+        .then((movie) => res.send(movie))
+        .catch((error) => res.send(error))
+        res.redirect('/movies')
+    },
+    edit: function(req, res) {
+        db.Movie.findByPk(req.params.id)
+        .then((movie) =>{
+            res.render('moviesEdit', {
+                movie
+            })
+        })
+        .catch((error) => res.send(error))
+    },
+    update: function (req,res) {
+        db.Movie.update({
+            title: req.body.title,
+            rating: req.body.rating,
+            length: req.body.length,
+            awards: req.body.awards,
+            release_date: req.body.release_date,
+        }, {
+            where: {
+                id: req.params.id
+            }
+        })
+        .catch((error) => res.send(error))
+        res.redirect('/movies')
+    },
+    delete: function (req, res) {
+        db.Movie.findByPk(req.params.id)
+        .then((movie) => {
+            res.render('moviesDelete', {
+                movie
+            })
+        })
+        .catch((error) => res.send(error))
+    },
+    destroy: function (req, res) {
+        db.Movie.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(() => res.send())
+        .catch((error) => res.send(error))
+        res.redirect('/movies')
+        
+    }
+
+}
+
+module.exports = moviesController;
---
+++ src/validations/moviesValidations.js
@@ -0,0 +1,8 @@
+const {check} = require('express-validator');
+
+module.exports = [
+    check("title").notEmpty().withMessage("Requerido"),
+    check("rating").notEmpty().withMessage("Requerido"),
+    check("awards").notEmpty().withMessage("Requerido"),
+    check("release_date").notEmpty().withMessage("Requerido")
+]
